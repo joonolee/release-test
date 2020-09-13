25d073efea7ccd95a23cb1705b8157b1afcba62f
joonolee <joono_lee@naver.com>
2020-09-14 00:02:01 +0900

    릴리즈: :bookmark: v8.0.0 릴리즈

--- new_release.sh
+++ new_release.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 # 새 버전번호 입력
-read -p "릴리즈할 버전번호(예: 1.0.0+1)를 입력하세요:  " NEW_VERSION
+read -p "릴리즈할 버전번호(예: 1.0.0)를 입력하세요:  " NEW_VERSION
 
 # 버전번호 형식 체크
-if [[ ! $NEW_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+\+[0-9]+ ]]
+if [[ ! $NEW_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]
 then
-    echo "버전은 {MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION}+{FIX_VERSION} 형식이어야 함"
+    echo "버전은 {MAJOR_VERSION}.{MINOR_VERSION}.{PATCH_VERSION} 형식이어야 함"
     exit 1
 fi
 
 # git 최신 버전으로 pull
 git pull
 
-# pubspec.yaml 파일에 버전 번호 변경
-sed -i "s/^version: .*$/version: ${NEW_VERSION}/" pubspec.yaml
+# package.json 파일에 버전 번호 변경
+sed -i "s/\"version\": \".*\",$/\"version\": \"${NEW_VERSION}\",/" package.json
 
 # git 저장소에 push
 git add .
-git commit -m "릴리즈: :bookmark: v${NEW_VERSION%\+*} 릴리즈"
-git tag v${NEW_VERSION%\+*}
-git push
+git commit -m "릴리즈: :bookmark: v${NEW_VERSION} 릴리즈"
+git tag v${NEW_VERSION}
 git push --tags
